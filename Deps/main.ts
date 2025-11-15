79ec1db77bc5ed0d0d68f0e35b187b33d1d4110f
Raz <205166971+RazAPIx64@users.noreply.github.com>
2025-11-15 16:22:53 +0200

    add assert and analyze func

--- Deps/main.ts
+++ Deps/main.ts
@@ -8,5 +8,11 @@ export namespace rUNC {
 
         // [rUNCCore::Deps::SignConnection(RBXScriptSignal, Function)]
         export const sign_connection = <T extends (...args: unknown[]) => void>(c: RBXScriptSignal<T>, f: T) => c.Connect(f);
+
+        // [rUNCCore::Deps::Assert(Condition: Boolean, Function: string, Message: string)]
+        export const As = (con: boolean, fn: string, msg: string) {}
+
+        // [rUNCCore::Deps::Analyze(Function: string, Callback: Function)]
+        export const Analyze = (fn: string, callback: () => void) {}
     }
 }
