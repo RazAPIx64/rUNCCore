46103d464c1b218180ac03bbb5a011af809e5ec6
Raz <205166971+RazAPIx64@users.noreply.github.com>
2025-11-15 14:11:08 +0200

    add startup

--- Startup/main.ts
+++ Startup/main.ts
@@ -1,1 +1,23 @@
+export namespace rUNC
+{
+    export namespace startup
+    {
+        // [rUNC::Startup::PlaceDependencies]
+        export let place_dependencies = {
+            id: 10104167448374972,
+            teleport_service: game.GetService('TeleportService'),
+        };
 
+        // [rUNC::Startup::DataModel]
+        export let data_model = game;
+
+        // [rUNC::Startup::DevMode]
+        export let developer_mode = true;
+
+        // [rUNC::Startup::GlobalEnvironmentCheck]
+        export let does_genv_work = pcall(getgenv);
+
+        // [rUNC::Startup::AlternativeGlobalEnvironment]
+        export let work_around;
+    }
+}
