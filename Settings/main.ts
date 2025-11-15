a4fa4993c51712f59bd8be788ba7d52c2505d7bb
Raz <205166971+RazAPIx64@users.noreply.github.com>
2025-11-15 14:16:30 +0200

    add settings

--- Settings/main.ts
+++ Settings/main.ts
@@ -2,7 +2,7 @@ export namespace rUNC {
     export namespace settings {
         // [rUNC::Settings::UseInterface]
         export let use_interface = true;
-
+        
         // [rUNC::Settings::DbgTests]
         export let dbg_tests = false;
     }
